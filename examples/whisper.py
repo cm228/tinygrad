d3018df6e40919c20d90b68c1f86965d48d04139
cm228 <connor+github@mccolgan.me>
2025-05-29 13:45:13 +0000

    print segments for debug

--- examples/whisper.py
+++ examples/whisper.py
@@ -1,6 +1,6 @@
 # thanks to https://github.com/openai/whisper for a good chunk of MIT licensed code
 
-import sys, base64, multiprocessing, itertools, collections
+import sys, base64, multiprocessing, itertools, collections, json
 from typing import Optional, Union, Literal, List
 
 from tinygrad import Tensor, TinyJit, Variable, nn
@@ -272,6 +272,18 @@ def transcribe_waveform(model: Whisper, enc, waveforms, truncate=False, use_time
       pos = ctx.shape[-1] - 1
       if (next_tokens == eot).all(): break
     return ctx
+  
+  def tt2sec(tok): return float(enc.decode([tok])[2:-2])
+  
+  def process_ctx(toks, seek):
+    i, r, c = np.where(toks == start_tokens[-1])[0][0] + 1, [], []
+    for t in toks[i:]:
+        if t > enc._special_tokens['<|notimestamps|>'] and len(c) > 1:
+            r.append({'text': enc.decode(c[1:]).strip(), 'start': seek + tt2sec(c[0]), 'end': seek + tt2sec(t)})
+            c = []
+        else: c.append(t)
+    if not r: r.append({'text': enc.decode(c[len(start_tokens):]), 'start': seek, 'end': seek + 30})
+    return r
 
   def gettexttoks(line): return [tok for tok in line if tok < eot or tok > enc._special_tokens["<|notimestamps|>"]][-nsample+len(start_tokens):]
   start_tokens = [enc._special_tokens["<|startoftranscript|>"]]
@@ -286,6 +298,7 @@ def transcribe_waveform(model: Whisper, enc, waveforms, truncate=False, use_time
 
   ctx = np.tile(start_tokens, (model.batch_size,1))
   transcriptions = [[] for _ in waveforms]
+  all_segments = [[] for _ in waveforms]
 
   for curr_frame in range(0, log_spec.shape[-1], FRAMES_PER_SEGMENT):
     encoded_audio = model.encoder.encode(Tensor(log_spec[:, :, curr_frame:curr_frame + FRAMES_PER_SEGMENT]))
@@ -293,11 +306,14 @@ def transcribe_waveform(model: Whisper, enc, waveforms, truncate=False, use_time
     if all(len(c) == len(ctx[0]) for c in ctx): ctx = inferloop(np.array(ctx), encoded_audio)
     else: ctx = [inferloop((np.array([c]*model.batch_size)), encoded_audio)[i] for i,c in enumerate(ctx)]
 
-    for i, (res, arr) in enumerate(zip(transcriptions, ctx)):
-      if curr_frame*HOP_LENGTH <= len(waveforms[i]):res.extend(arr[np.where(arr == start_tokens[-1])[0][0]+1:eoti[0] if len (eoti:=np.where(arr == eot)[0]) else None])
+    for i, (res, segs, arr) in enumerate(zip(transcriptions, all_segments, ctx)):
+      if curr_frame*HOP_LENGTH <= len(waveforms[i]):
+        res.extend(arr[np.where(arr == start_tokens[-1])[0][0]+1:eoti[0] if len (eoti:=np.where(arr == eot)[0]) else None])
+        segs.extend(process_ctx(arr, round(curr_frame * HOP_LENGTH / RATE / 0.02) * 0.02))
     ctx = [[enc._special_tokens['<|startofprev|>']]+gettexttoks(cs)+start_tokens for cs in ctx]
 
   transcriptions = list(map(lambda tokens: enc.decode(tokens).strip(), transcriptions))
+  json.dump(all_segments[0], open('output.json', 'w'), indent=2)
   return transcriptions if len(transcriptions) > 1 else transcriptions[0]
 
 CHUNK = 1600
