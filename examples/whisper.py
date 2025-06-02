cf16ab78b63d89ad956d248dfe08e35693a5f0e7
cm228 <connor+github@mccolgan.me>
2025-06-02 13:41:42 +0000

    fix 6

--- examples/whisper.py
+++ examples/whisper.py
@@ -342,7 +342,7 @@ def transcribe_waveform(model: Whisper, enc, waveforms, use_beam=False, use_time
     if not r: 
       r.append({'text': enc.decode(c[1:]), 'start': seek, 'end': seek + 30})
       c = []
-    if len(c)>1 and enc.decode(gettexttoks(c[1:]))>0:
+    if len(c)>1 and len(enc.decode(gettexttoks(c[1:])))>0:
       r.append({'text': enc.decode(gettexttoks(c[1:])), 'start': seek + tt2sec(c[0]), 'end': seek + tt2sec(c[0])+30})
     return r
   
