6e25cadabd73a04888ff057f2344fa60baed0ba0
cm228 <connor+github@mccolgan.me>
2025-06-02 11:50:43 +0000

    safe seek_fn

--- examples/whisper.py
+++ examples/whisper.py
@@ -328,6 +328,10 @@ def transcribe_waveform(model: Whisper, enc, waveforms, use_beam=False, use_time
 
   def gettexttoks(line): return [tok for tok in line if tok < eot or tok > enc._special_tokens["<|notimestamps|>"]][-nsample+len(start_tokens):]
   def tt2sec(tok): return float(enc.decode([tok])[2:-2])
+  def seek_fn(ctx):
+    last_timestamp = ctx[0][-(len(start_tokens)+1)]
+    if last_timestamp>enc._special_tokens["<|notimestamps|>"]: return int(tt2sec(last_timestamp) / 30 * FRAMES_PER_SEGMENT)
+    else: return FRAMES_PER_SEGMENT
   start_tokens = [enc._special_tokens["<|startoftranscript|>"]]
   if model.is_multilingual:
     # TODO detect language
@@ -351,7 +355,7 @@ def transcribe_waveform(model: Whisper, enc, waveforms, use_beam=False, use_time
     for i, (res, arr) in enumerate(zip(transcriptions, ctx)):
       if curr_frame*HOP_LENGTH <= len(waveforms[i]):res.extend(arr[np.where(arr == start_tokens[-1])[0][0]+1:eoti[0] if len (eoti:=np.where(arr == eot)[0]) else None])
     ctx = [[enc._special_tokens['<|startofprev|>']]+gettexttoks(cs)+start_tokens for cs in ctx]
-    curr_frame += FRAMES_PER_SEGMENT if not use_timestamps else int(tt2sec(ctx[0][-(len(start_tokens)+1)]) / 30 * FRAMES_PER_SEGMENT)
+    curr_frame += FRAMES_PER_SEGMENT if not use_timestamps else seek_fn(ctx)
     print(curr_frame)
 
   transcriptions = list(map(lambda tokens: enc.decode(tokens).strip(), transcriptions))
