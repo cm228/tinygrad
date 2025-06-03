6a036cccca4d1417893df22369f161ec5e55b341
cm228 <connor+github@mccolgan.me>
2025-06-03 14:21:03 +0000

    reset

--- examples/whisper.py
+++ examples/whisper.py
@@ -264,7 +264,7 @@ def transcribe_waveform(model: Whisper, enc, waveforms, use_beam=False, use_time
     start, max_first = enc._special_tokens['<|0.00|>'], enc._special_tokens['<|1.00|>']
     for i, row in enumerate(ctx):
       last, penult = row[-1], (row[-2] if row[-1]!=start_tokens[-1] else None)
-      if last == start_tokens[-1]: logits[i, (list(range(start))+list(range(max_first, enc._special_tokens['<|30.00|>'])) if use_timestamps else enc.encode(' ') + [eot])] = -np.inf
+      if last == start_tokens[-1]: logits[i, (list(range(start)) if use_timestamps else enc.encode(' ') + [eot])] = -np.inf
       elif last > start:
         if penult is not None and (penult==start_tokens[-1] or penult>=start): logits[i, start:] = -np.inf
         else: logits[i, np.r_[:eot, start:last]] = -np.inf # np.r_[:eot, start:last]
